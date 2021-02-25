078bb08540a561a85386525903288720ed3ba8e0
quentinmay <quentinemay@gmail.com>
2021-02-25 07:24:44 +0000

    working on basic example. still not working well

--- index.js
+++ index.js
@@ -2,8 +2,25 @@ const {Builder, By, Key, until} = require('selenium-webdriver');
 const chrome = require('selenium-webdriver/chrome');
 const chromedriver = require('chromedriver');
 
-chrome.setDefaultService(new chrome.ServiceBuilder(chromedriver.path).build());
+//chrome.setDefaultService(new chrome.ServiceBuilder(chromedriver.path).build());
 
+async function example() {
+
+let driver = new webdriver.Builder()
+    .forBrowser('chrome')
+    .setChromeOptions(/* ... */)
+    .build();
+    
+      await driver.get('http://www.google.com/ncr');
+      
+      await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
+      await driver.wait(until.titleIs('webdriver - Google Search'), 1000);
+      console.log(driver);
+      
+      await driver.quit();
+
+  }
+    
 /*
 const screen = {
   width: 640,
