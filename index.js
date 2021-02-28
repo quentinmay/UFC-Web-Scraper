2ae6f61f73ba15a6e97c4a7069e80cb1efbfd40d
quentinmay <quentinemay@gmail.com>
2021-02-28 15:28:24 -0800

    Changed to UFC scraping. Successfuly scrapes ufc website for matchups and winrates formatted in json.

--- index.js
+++ index.js
@@ -1,14 +1,45 @@
-const {Builder, By, Key, until} = require('selenium-webdriver');
-const chrome = require('selenium-webdriver/chrome');
-const chromedriver = require('chromedriver');
+//const {Builder, By, Key, until} = require('selenium-webdriver');
+//const chrome = require('selenium-webdriver/chrome');
+//const chromedriver = require('chromedriver');
+const axios = require('axios').default;
+const cheerio = require('cheerio');
+var fs = require('fs');
+
+getAxiousUFC();
+//
+async function getAxiousUFC() {
+  const time = Date.now();
+  const options = {
+    headers: {'authority': "io.oddsshark.com",
+    'method': "GET",
+    'path': `/ticker/ufc?_=${time}`,
+    'scheme': "https",
+    'accept': "*/*",
+    'accept-encoding': "gzip, deflate, br",
+    'accept-language': "en-US,en;q=0.9,ja;q=0.8",
+    'dnt': "1",
+    'origin': "https://www.oddsshark.com",
+    'referer': "https://www.oddsshark.com/",
+    'sec-fetch-dest': "empty",
+    'sec-fetch-mode': "cors",
+    'sec-fetch-site': "same-site",
+    'user-agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.190 Safari/537.36"}
+  }
+  var response = await axios.get(`https://io.oddsshark.com/ticker/ufc?_=${time}`, options)
+  
+  fs.writeFileSync("matchData.json", JSON.stringify(response.data, null, 2));
+  console.log(response.data);
+
+}
 
 //chrome.setDefaultService(new chrome.ServiceBuilder(chromedriver.path).build());
 
+/*
 async function example() {
 
 let driver = new webdriver.Builder()
     .forBrowser('chrome')
-    .setChromeOptions(/* ... */)
+    .setChromeOptions()
     .build();
     
       await driver.get('http://www.google.com/ncr');
@@ -20,7 +51,9 @@ let driver = new webdriver.Builder()
       await driver.quit();
 
   }
-    
+    */
+
+
 /*
 const screen = {
   width: 640,
